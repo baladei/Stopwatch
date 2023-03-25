d4f9e353b8bea84b976acafb63693b880806a62a
Fernando Baladei <f.baladei@gmail.com>
2023-03-25 08:02:09 -0300

    starting responsivity of seconds and minutes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,44 @@ import './App.css'
 
 function App() {
 
+  const minutesEL = document.querySelector("#minutes")
+  const secondsEL = document.querySelector("#seconds")
+  const millisecondsEL = document.querySelector("#milliseconds")
+  const startEL = document.querySelector("#start")
+  const pauseEL = document.querySelector("#pause")
+  const resumeEL = document.querySelector("#resume")
+  const resetEL = document.querySelector("#reset")
+
+  let interval;
+  let minutes = 0;
+  let seconds = 0;
+  let milliseconds = 0;
+  let isPaused = false
+
+  function startTimer() {
+
+    interval = setInterval (() => {
+      if(!isPaused) {
+      milliseconds += 10;
+
+      if(milliseconds === 1000) {
+        seconds++;
+        milliseconds = 0;
+      }
+
+      if(seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }}
+
+      minutesEL.textContent = minutes
+      secondsEL.textContent = seconds
+      millisecondsEL.textContent = milliseconds
+
+    }, 10)
+
+  }
+
   return (
     <div className="App">
       <h1>Cronômetro - React JS</h1>
@@ -15,7 +53,7 @@ function App() {
       </div>
 
         <div className="buttons">
-          <button className="btn" id="start">Iniciar</button>
+          <button className="btn" id="start" onClick={startTimer}>Iniciar</button>
           <button className="btn" id="pause">Pausar</button>
           <button className="btn" id="resume">Continuar</button>
           <button className="btn" id="reset">Resetar</button>
