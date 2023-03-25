f251511c050ad61068a51484789a7a5ca0ef1031
Fernando Baladei <f.baladei@gmail.com>
2023-03-25 11:09:30 -0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import './App.css'
 
-function App() {
-
-  const minutesEL = document.querySelector("#minutes")
-  const secondsEL = document.querySelector("#seconds")
-  const millisecondsEL = document.querySelector("#milliseconds")
-  const startEL = document.querySelector("#start")
-  const pauseEL = document.querySelector("#pause")
-  const resumeEL = document.querySelector("#resume")
-  const resetEL = document.querySelector("#reset")
+function App() {  
 
   let interval;
   let minutes = 0;
   let seconds = 0;
   let milliseconds = 0;
-  let isPaused = false
+  let isChange = true;
 
   function startTimer() {
 
     interval = setInterval (() => {
-      if(!isPaused) {
+      if(isChange) {
       milliseconds += 10;
 
       if(milliseconds === 1000) {
@@ -32,12 +24,59 @@ function App() {
         seconds = 0;
       }}
 
-      minutesEL.textContent = minutes
-      secondsEL.textContent = seconds
-      millisecondsEL.textContent = milliseconds
+      const minutesEL = document.querySelector("#minutes")
+      const secondsEL = document.querySelector("#seconds")
+      const millisecondsEL = document.querySelector("#milliseconds")
+
+      minutesEL.textContent = formatTimer(minutes)
+      secondsEL.textContent = formatTimer(seconds)
+      millisecondsEL.textContent = formatMilliseconds(milliseconds)
 
     }, 10)
 
+    start.style.display = "none";
+    pause.style.display = "block";
+  }
+
+  function pauseTimer () {
+    isChange = false;
+    pause.style.display = "none";
+    resume.style.display = "block"
+  }
+
+  function resumeTimer () {
+    isChange = true;
+    pause.style.display = "block";
+    resume.style.display = "none";
+  }
+
+  function resetTimer () {
+
+      const minutesEL = document.querySelector("#minutes")
+      const secondsEL = document.querySelector("#seconds")
+      const millisecondsEL = document.querySelector("#milliseconds")
+    
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEL.textContent = '00'
+    secondsEL.textContent = '00'
+    millisecondsEL.textContent = '000'
+
+    start.style.display = "block"
+    resume.style.display = "none"
+    pause.style.display = "none"
+    isChange = true
+  }
+
+  function formatTimer (time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  function formatMilliseconds (time) {
+    return time < 100 ? `${time}`.padStart(3, "0") : time
   }
 
   return (
@@ -54,9 +93,9 @@ function App() {
 
         <div className="buttons">
           <button className="btn" id="start" onClick={startTimer}>Iniciar</button>
-          <button className="btn" id="pause">Pausar</button>
-          <button className="btn" id="resume">Continuar</button>
-          <button className="btn" id="reset">Resetar</button>
+          <button className="btn" id="pause" onClick={pauseTimer}>Pausar</button>
+          <button className="btn" id="resume" onClick={resumeTimer}>Continuar</button>
+          <button className="btn" id="reset" onClick={resetTimer}>Resetar</button>
         </div>
 
     </div>
